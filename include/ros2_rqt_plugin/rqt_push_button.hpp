927fe2c9ab7e7c253e44c4f7bfe465a4d26015da
chanjl <chanjl@i2r.a-star.edu.sg>
2021-10-08 18:06:26 +0800

    add header file for rqt plugin

---
+++ include/ros2_rqt_plugin/rqt_push_button.hpp
@@ -0,0 +1,39 @@
+#ifndef PUSH_BUTTON_H__
+#define PUSH_BUTTON_H__
+
+// ROS2
+#include <rclcpp/rclcpp.hpp>
+
+// msgs
+#include <std_msgs/msg/bool.hpp>
+
+// Rqt
+#include <rqt_gui_cpp/plugin.h>
+
+// Qt
+#include <QWidget>
+
+// Custom UI
+// #include <ros2_rqt_plugin/ui_rqt_push_button.h>
+
+namespace rqt_plugin
+{
+  class pushButton : public rqt_gui_cpp::Plugin
+  {
+    Q_OBJECT
+    public:
+      pushButton();
+
+      virtual void initPlugin(qt_gui_cpp::PluginContext& context);
+
+      virtual void shutdownPlugin();
+
+      virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const;
+
+      virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings);
+
+      virtual ~pushButton();
+  };
+}  // namespace rqt_plugin
+
+#endif  // PUSH_BUTTON_H__
