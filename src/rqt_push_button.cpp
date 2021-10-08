d7cd9424af2f13ada287623521310de3c93608f9
chanjl <chanjl@i2r.a-star.edu.sg>
2021-10-08 20:04:16 +0800

    feat: finish off with publish feature

--- src/rqt_push_button.cpp
+++ src/rqt_push_button.cpp
@@ -2,7 +2,7 @@
 #include <pluginlib/class_list_macros.hpp>
 #include <QStringList>
 
-PLUGINLIB_EXPORT_CLASS(rqt_plugin::pushButton, rqt_gui_cpp::Plugin);
+PLUGINLIB_EXPORT_CLASS(rqt_plugin::pushButton, rqt_gui_cpp::Plugin)
 
 namespace rqt_plugin
 {
@@ -14,10 +14,28 @@ namespace rqt_plugin
 
     void pushButton::initPlugin(qt_gui_cpp::PluginContext& context)
     {
+        // Access standalone command line arguments
+        QStringList argv = context.argv();
+
+        // Create QWidget
         widget_ = new QWidget();
+
+        // Extend the widget with all attributes and children from UI file
         ui_.setupUi(widget_);
 
+        // add widget to the user interface
         context.addWidget(widget_);
+
+        // ROS2 related declaration
+        button1_pub_ = node_->create_publisher<std_msgs::msg::Bool>("button1", 1);
+        button2_pub_ = node_->create_publisher<std_msgs::msg::Bool>("button2", 1);
+
+        // Prepare msg
+        msg_.data = true;
+
+        // Connect with Qt Widget
+        connect(ui_.pushButton_1, SIGNAL(pressed()), this, SLOT(button1Callback()));
+        connect(ui_.pushButton_2, SIGNAL(pressed()), this, SLOT(button2Callback()));
     }
 
     void pushButton::shutdownPlugin()
@@ -40,4 +58,15 @@ namespace rqt_plugin
         if(widget_)
             delete widget_;
     }
+
+    void pushButton::button1Callback()
+    {
+        button1_pub_->publish(msg_);
+    }
+
+    void pushButton::button2Callback()
+    {
+        button2_pub_->publish(msg_);
+    }
+
 } // namespace rqt_plugin
