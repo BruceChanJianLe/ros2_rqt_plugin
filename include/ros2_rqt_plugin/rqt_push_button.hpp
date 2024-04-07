ade9a71cac5327b82749700cd9595d0724b7473b
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2024-04-07 22:13:15 +0800

    feat: auto-connect button signal

--- include/ros2_rqt_plugin/rqt_push_button.hpp
+++ include/ros2_rqt_plugin/rqt_push_button.hpp
@@ -18,34 +18,42 @@
 
 namespace rqt_plugin
 {
+  // To use autoconnect signals and slots
+  class PushButtonWidget : public QWidget
+  {
+  Q_OBJECT
+  public:
+    explicit PushButtonWidget(rclcpp::Node::SharedPtr node);
+    virtual ~PushButtonWidget();
+
+  private slots:
+      void on_pushButton1_clicked();
+      void on_pushButton2_clicked();
+
+  private:
+      rclcpp::Node::SharedPtr node_;
+
+  protected:
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr button1_pub_;
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr button2_pub_;
+    std_msgs::msg::Bool msg_;
+  };
+
   class pushButton : public rqt_gui_cpp::Plugin
   {
     Q_OBJECT
     public:
       pushButton();
-
-      virtual void initPlugin(qt_gui_cpp::PluginContext& context) override;
-
-      virtual void shutdownPlugin() override;
-
-      virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const override;
-
-      virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings) override;
-
       virtual ~pushButton();
 
-      private:
-        Ui::gui ui_;
-        QWidget * widget_;
+      virtual void initPlugin(qt_gui_cpp::PluginContext& context) override;
+      virtual void shutdownPlugin() override;
+      virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const override;
+      virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings) override;
 
-      protected:
-        rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr button1_pub_;
-        rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr button2_pub_;
-        std_msgs::msg::Bool msg_;
-      
-      protected slots:
-        void button1Callback();
-        void button2Callback();
+    private:
+      Ui::gui ui_;
+      PushButtonWidget *widget_;
   };
 }  // namespace rqt_plugin
 
--- src/rqt_push_button.cpp
+++ src/rqt_push_button.cpp
@@ -1,72 +1,78 @@
 #include "ros2_rqt_plugin/rqt_push_button.hpp"
 #include <pluginlib/class_list_macros.hpp>
 #include <QStringList>
+#include <qnamespace.h>
 
 PLUGINLIB_EXPORT_CLASS(rqt_plugin::pushButton, rqt_gui_cpp::Plugin)
 
 namespace rqt_plugin
 {
-    pushButton::pushButton()
-    :   rqt_gui_cpp::Plugin()
-    ,   widget_(0)
+  PushButtonWidget::PushButtonWidget(rclcpp::Node::SharedPtr node)
+    : node_{node}
+  {
+    // ROS2 related declaration
+    button1_pub_ = node_->create_publisher<std_msgs::msg::Bool>("button1", 1);
+    button2_pub_ = node_->create_publisher<std_msgs::msg::Bool>("button2", 1);
+
+    // Prepare msg
+    msg_.data = true;
+  }
+
+  PushButtonWidget::~PushButtonWidget() {}
+
+  void PushButtonWidget::on_pushButton1_clicked()
+  {
+    RCLCPP_INFO_STREAM(node_->get_logger(), "Published to button1 topic!");
+    button1_pub_->publish(msg_);
+  }
+
+  void PushButtonWidget::on_pushButton2_clicked()
+  {
+    RCLCPP_INFO_STREAM(node_->get_logger(), "Published to button2 topic!");
+    button2_pub_->publish(msg_);
+  }
+
+  pushButton::pushButton()
+    : rqt_gui_cpp::Plugin()
+    , widget_(0)
+  {
+  }
+
+  void pushButton::initPlugin(qt_gui_cpp::PluginContext& context)
+  {
+    // Access standalone command line arguments
+    QStringList argv = context.argv();
+
+    // Create QWidget
+    widget_ = new PushButtonWidget(node_);
+
+    // Extend the widget with all attributes and children from UI file
+    ui_.setupUi(widget_);
+
+    // add widget to the user interface
+    context.addWidget(widget_);
+  }
+
+  void pushButton::shutdownPlugin()
+  {
+    ;
+  }
+
+  void pushButton::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
+  {
+    ;
+  }
+
+  void pushButton::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings)
+  {
+    ;
+  }
+
+  pushButton::~pushButton()
+  {
+    if(widget_)
     {
+      delete widget_;
     }
-
-    void pushButton::initPlugin(qt_gui_cpp::PluginContext& context)
-    {
-        // Access standalone command line arguments
-        QStringList argv = context.argv();
-
-        // Create QWidget
-        widget_ = new QWidget();
-
-        // Extend the widget with all attributes and children from UI file
-        ui_.setupUi(widget_);
-
-        // add widget to the user interface
-        context.addWidget(widget_);
-
-        // ROS2 related declaration
-        button1_pub_ = node_->create_publisher<std_msgs::msg::Bool>("button1", 1);
-        button2_pub_ = node_->create_publisher<std_msgs::msg::Bool>("button2", 1);
-
-        // Prepare msg
-        msg_.data = true;
-
-        // Connect with Qt Widget
-        connect(ui_.pushButton_1, SIGNAL(pressed()), this, SLOT(button1Callback()));
-        connect(ui_.pushButton_2, SIGNAL(pressed()), this, SLOT(button2Callback()));
-    }
-
-    void pushButton::shutdownPlugin()
-    {
-        ;
-    }
-
-    void pushButton::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
-    {
-        ;
-    }
-
-    void pushButton::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const qt_gui_cpp::Settings& instance_settings)
-    {
-        ;
-    }
-
-    pushButton::~pushButton()
-    {
-        if(widget_)
-            delete widget_;
-    }
-
-    void pushButton::button1Callback()
-    {
-        button1_pub_->publish(msg_);
-    }
-
-    void pushButton::button2Callback()
-    {
-        button2_pub_->publish(msg_);
-    }
-
+  }
 } // namespace rqt_plugin
